50c90e836b2ca1f508e1591d769cafb86b44e6e5
wajahatiqbal <wajahatiqbalwaji@gmail.com>
2023-01-21 14:31:14 +0500

    Add blur hash painter as lib

--- blurhash/build.gradle.kts
+++ blurhash/build.gradle.kts
@@ -32,6 +32,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 }
 
 dependencies {
