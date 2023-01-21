4b94b342592ce98fe4c6c56b6eea7be381d6ac74
wajahatiqbal <wajahatiqbalwaji@gmail.com>
2023-01-21 14:13:15 +0500

    Add blur hash painter as lib

--- blurhash/build.gradle.kts
+++ blurhash/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    `maven-publish`
 }
 
 android {
@@ -42,4 +43,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.wajahat-iqbal"
+            artifactId = "blur-hash-painter"
+            version = "1.0"
+
+        }
+    }
 }
